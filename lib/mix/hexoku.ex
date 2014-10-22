053cbffdde6366f37d6aa1ca7d1e5704a7f349fe
Jon Gretar <jongretar@jongretar.com>
2014-10-22 21:58:21 +0000

    Mix doc nicer

--- lib/mix/hexoku.ex
+++ lib/mix/hexoku.ex
@@ -1,46 +1,71 @@
 defmodule Mix.Tasks.Hexoku do
-  use Mix.Task
-  @moduledoc """
+	use Mix.Task
+	@moduledoc """
 
-  ## Heroku Mix Tasks:
+	Hexoku adds some common Heroku commands to your Mix. It relies on [Heroku Toolbet](toolbelt.heroku.com) being
+	installed and logged in.
 
-    mix hexoku.app  [--app APP]                   # Get application info
-    mix hexoku.config [--app APP]                 # List configuration variables
-    mix hexoku.log [--app APP] [--tail]           # Show applications log
-    mix hexoku --help                             # This help
-  """
-  @shortdoc "List Hexoku tasks"
+	The application name is read from the heroku_app key in your `mix.exs` files project.
+	If that is not defined it falls back to the :app key.
 
-  def parse_options(argv, switches) do
-    {parsed, argv, errors} = OptionParser.parse(argv, strict: switches)
-    unless Keyword.get(parsed, :app, false) do
-      app = Mix.Project.config[:heroku_app] || Mix.Project.config[:app]
-      parsed = Keyword.put(parsed, :app, app)
-    end
-    if length(errors) > 0 do
-      for {flag, _} <- errors, do: Mix.shell.error("Error with flag "<>flag)
-      System.halt(1)
-    else
-      {parsed, argv}
-    end
-  end
+		def project do
+			[
+				app: :uberweb,
+				heroku_app: "uberweb-staging"
+				version: "0.0.1",
+				elixir: "~> 1.0.0",
+				deps: deps
+			]
+		end
 
-  def run([]) do
-    help
-  end
+	### Examples
 
-  def run(["--help"]) do
-    help
-  end
+		bash> mix hexoku.config
+			DOWNSTREAM_APP = myapp-production
+			REDIS_HOST = somehost.redistogo.com
 
-  defp help do
-    Mix.shell.info """
-    Help:
 
-    mix hexoku.app  [--app APP]                   # Get application info
-    mix hexoku.config [--app APP]                 # List configuration variables
-    mix hexoku.log [--app APP] [--tail]           # Show applications log
-    mix hexoku --help                             # This help
-    """
-  end
+	## Heroku Mix Tasks:
+
+		mix hexoku.app	[--app APP]             # Get application info
+		mix hexoku.config [--app APP]           # List configuration variables
+		mix hexoku.log [--app APP] [--tail]     # Show applications log
+		mix hexoku --help                       # This help
+	"""
+	@shortdoc "List Hexoku tasks"
+
+	@doc false
+	def parse_options(argv, switches) do
+		{parsed, argv, errors} = OptionParser.parse(argv, strict: switches)
+		unless Keyword.get(parsed, :app, false) do
+			app = Mix.Project.config[:heroku_app] || Mix.Project.config[:app]
+			parsed = Keyword.put(parsed, :app, app)
+		end
+		if length(errors) > 0 do
+			for {flag, _} <- errors, do: Mix.shell.error("Error with flag "<>flag)
+			System.halt(1)
+		else
+			{parsed, argv}
+		end
+	end
+
+	@doc false
+	def run([]) do
+		help
+	end
+
+	def run(["--help"]) do
+		help
+	end
+
+	defp help do
+		Mix.shell.info """
+		Help:
+
+		mix hexoku.app	[--app APP]									 # Get application info
+		mix hexoku.config [--app APP]								 # List configuration variables
+		mix hexoku.log [--app APP] [--tail]					 # Show applications log
+		mix hexoku --help														 # This help
+		"""
+	end
 end
