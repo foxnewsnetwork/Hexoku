fcf774fd665b732262801bfd786dd400521d80d3
Jon Gretar <jongretar@jongretar.com>
2014-10-22 21:49:26 +0000

    Mix: Get formation information with app info

--- lib/mix/hexoku/app.ex
+++ lib/mix/hexoku/app.ex
@@ -2,6 +2,7 @@ defmodule Mix.Tasks.Hexoku.App do
 	use Mix.Task
 	alias Mix.Tasks.Hexoku, as: H
 	alias Hexoku.API.Apps
+	alias Hexoku.API.Formation
 	alias Hexoku.Response
 	@moduledoc false
 
@@ -14,14 +15,22 @@ defmodule Mix.Tasks.Hexoku.App do
 	def run(argv) do
 		{options, _} = H.parse_options(argv, @switches)
 		client = Hexoku.toolbelt
+
+		Mix.shell.info("App Name:       #{Keyword.get(options, :app)}")
 		%Response{status: 200, body: info} = client |> Apps.info(Keyword.get(options, :app))
-		Mix.shell.info("App Name:       #{info[:name]}")
 		Mix.shell.info("Region:         #{info[:region][:name]}")
 		Mix.shell.info("Stack:          #{info[:stack][:name]}")
 		Mix.shell.info("Created:        #{info[:created_at]}")
 		Mix.shell.info("Updated:        #{info[:updated_at]}")
 		Mix.shell.info("Released:       #{info[:released_at]}")
 		Mix.shell.info("URL:            #{info[:web_url]}")
+
+		%Response{status: 200, body: formations} = client |> Formation.list(Keyword.get(options, :app))
+		Mix.shell.info("\nFormation:")
+		for formation <- formations do
+			Mix.shell.info("  #{formation[:quantity]} x #{formation[:type]}: #{formation[:command]} (#{formation[:size]})")
+		end
+
 		if info[:maintenance] == true do
 			Mix.shell.error("Application is in maintenance mode")
 		end
